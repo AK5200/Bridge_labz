79bc60b5cb7bd8ad731d36a1b300881fb54313a7
AK5200 <anupamkmr5200@gmail.com>
2024-05-31 10:25:16 +0530

    learning functions

---
+++ functions.js
@@ -0,0 +1,96 @@
+
+// function declaration
+hello(2); // can be called before declaration (function expressions are hoisted)
+function hello(v)
+{
+    console.log(`Hello ${v}`);
+}
+
+hello(1);
+
+// function expresssion
+//fun1()  // will give error as function expression are not hoisted
+
+var fun1 = function(){  //no need of giving name to the funtion
+    console.log("fun1");
+}
+fun1();
+
+
+// arrow function
+
+var arrow = ()=>{
+    // arrow fun syntax
+    console.log("arrow")
+}
+
+arrow();
+
+var arrow1 = (a)=> {
+    // parameterized arrow
+}
+
+var arrow2 = a => {
+    // can omit parentheses if fun has only one parameter.
+}
+
+
+
+let fun = n => n < 2 ? 1 : n * fun(n-1);
+console.log(fun(3));
+
+
+function map(f,a)
+{
+    const result = new Array(a.length);
+    for(let i=0;i<a.length;i++)
+        {
+            result[i] = f(a[i]);
+        }
+
+        return result;
+}
+
+const cube = function(x)
+{
+    return x*x*x;
+}
+
+const num = [1,2,3,4];
+console.log(map(cube,num));
+
+
+// function based on some condition
+
+let obj = {
+    make:"range rover"
+}
+
+var cond = 0;
+if(cond == 0)
+    {
+        myfun = function (obj)
+        {
+            obj.make = "Toyota";
+        }
+    }
+
+    myfun(obj);
+    console.log(obj.make)
+
+
+
+const foo = function bar() {
+    console.log(1);
+}
+
+foo();
+//bar();  // not accessible outside the function
+  
+
+
+
+
+
+
+
